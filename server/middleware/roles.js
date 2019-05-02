2b50ca3f4439e8a8177f100048ac75b21873a227
Silvan Knecht <knecht.silvan@gmail.com>
2019-05-02 10:02:33 +0200

    bugfix

--- server/middleware/roles.js
+++ server/middleware/roles.js
@@ -1,7 +1,7 @@
 const logger = require("./logger");
 
 function isAdmin(req, res, next) {
-  if (req.user.role.includes("admin")) {
+  if (req.user.roles.includes("admin")) {
     logger.debug("im an admin");
     logger.debug(req.user.roles);
     next(null, req.body);
