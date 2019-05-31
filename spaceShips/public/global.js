df7e6e3c86def2ffe3ad19e960f80495e2b3c401
enjoymrban <knecht.silvan@gmail.com>
2019-05-31 23:50:26 +0200

    new db, db enviromentvariable

--- spaceShips/public/global.js
+++ spaceShips/public/global.js
@@ -1,5 +1,5 @@
-//const url = "http://localhost:5000/";
-const url = "https://peaceful-tundra-32544.herokuapp.com/"
+const url = "http://localhost:5000/";
+//const url = "https://peaceful-tundra-32544.herokuapp.com/"
 //const url = "https://realspaceships.herokuapp.com/"
 
 let localStorage;
